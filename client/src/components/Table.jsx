6ad1b444b88e8360a3922e9001a6454786755e45
nileshkharat-git <nileshk262000@gmail.com>
2024-08-24 13:26:43 +0530

    Update Table layout

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,38 +1,54 @@
 import React, { useEffect, useState } from "react";
 
 const Table = () => {
-    const [products, setProducts] = useState([]);
-    const [page, setPage] = useState(1);
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
-    useEffect(() =>{
-        fetch('http://localhost:8000/api/products')
-        .then(response => response.json())
-        .then(response => setProducts(response))
-        .catch(err => console.error(err));
-    },[products, setProducts])
+  const fetchData = async () => {
+    fetch(`http://localhost:8000/api/products?page=${page}`)
+      .then((response) => response.json())
+      .then((response) => setProducts(response))
+      .catch((err) => console.error(err));
+  };
+  useEffect(() => {
+    fetchData();
+  }, [page]);
 
-    const fetchProducts = (page)=>{
-        fetch(`http://localhost:8000/api/products?page=${page}`)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response);
-            setProducts(response);
-        })
-        .catch(err => console.error(err));
+  const fetchProducts = (page) => {
+    fetch(`http://localhost:8000/api/products?page=${page}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setProducts(response);
+      })
+      .catch((err) => console.error(err));
+  };
+  const handleIncrement = () => {
+    if (page < 6) {
+      setPage((prev) => prev + 1);
+      fetchProducts(page);
     }
-    const handleIncrement = () => {
-        if(page <6){
-            setPage((prev)=>prev + 1);
-            fetchProducts(page);
-        }
+  };
+  const handleDecrement = () => {
+    if (page > 1) {
+      setPage((prev) => prev - 1);
+      fetchProducts(page);
     }
-    const handleDecrement = () => {
-        if(page > 1){
-            setPage((prev)=>prev - 1);
-            fetchProducts(page);
-        }
-    }
-    const months = [{"text":"Jan", value:1 }, {"text":"Feb", value:2}, {"text":"Mar", value:3}, {"text":"Apr", value:4}, {"text":"May", value:5 }, {"text":"Jun", value:6}, {"text":"Jul", value:7}, {"text":"Aug", value:8}, {"text":"Sep", value:9}, {"text":"Oct", value:10}, {"text":"Nov", value:11}, {"text":"Dec", value:12}]
+  };
+  const months = [
+    { text: "Jan", value: 1 },
+    { text: "Feb", value: 2 },
+    { text: "Mar", value: 3 },
+    { text: "Apr", value: 4 },
+    { text: "May", value: 5 },
+    { text: "Jun", value: 6 },
+    { text: "Jul", value: 7 },
+    { text: "Aug", value: 8 },
+    { text: "Sep", value: 9 },
+    { text: "Oct", value: 10 },
+    { text: "Nov", value: 11 },
+    { text: "Dec", value: 12 },
+  ];
   return (
     <div>
       <div className="flex items-center justify-between flex-col">
@@ -52,43 +68,52 @@ const Table = () => {
             ))}
           </select>
         </div>
-        <div className="overflow-x-auto">
-          <table className="w-full bg-white shadow-md rounded-xl mx-4">
-            <thead>
-              <tr className="bg-blue-gray-100 text-gray-700 uppercase">
-                <th className="py-3 px-4 text-left">Id</th>
-                <th className="py-3 px-4 text-left">Title</th>
-                {/* <th className="py-3 px-4 text-left">Descrition</th> */}
-                <th className="py-3 px-4 text-left">Price</th>
-                <th className="py-3 px-4 text-left">Category</th>
-                <th className="py-3 px-4 text-left">Sold</th>
-                {/* <th className="py-3 px-4 text-left">Image</th> */}
+      </div>
+
+      <div className="overflow-x-auto">
+        <table className="w-full bg-white shadow-md rounded-xl mx-4">
+          <thead>
+            <tr className="bg-blue-gray-100 text-gray-700 uppercase">
+              <th className="py-3 px-4 text-left">Id</th>
+              <th className="py-3 px-4 text-left">Title</th>
+              {/* <th className="py-3 px-4 text-left">Descrition</th> */}
+              <th className="py-3 px-4 text-left">Price</th>
+              <th className="py-3 px-4 text-left">Category</th>
+              <th className="py-3 px-4 text-left">Sold</th>
+              {/* <th className="py-3 px-4 text-left">Image</th> */}
+            </tr>
+          </thead>
+          <tbody className="text-blue-gray-900">
+            {products.map((product) => (
+              <tr className="border-b border-blue-gray-200">
+                <td className="py-3 px-4">{product.id}</td>
+                <td className="py-3 px-4">{product.title}</td>
+                {/* <td className="py-3 px-4 "><p>{product.description}</p></td> */}
+                <td className="py-3 px-4">{product.price} &#8377;</td>
+                <td className="py-3 px-4">{product.category}</td>
+                <td className="py-3 px-4">
+                  {product.sold ? "Sold" : "Not Sold"}
+                </td>
+                {/* <td className="py-3 px-4">{product.image}</td> */}
               </tr>
-            </thead>
-            <tbody className="text-blue-gray-900">
-                {products.map(product =>(
-                    <tr className="border-b border-blue-gray-200">
-                        <td className="py-3 px-4">{product.id}</td>
-                        <td className="py-3 px-4">{product.title}</td>
-                        {/* <td className="py-3 px-4 "><p>{product.description}</p></td> */}
-                        <td className="py-3 px-4">{product.price} &#8377;</td>
-                        <td className="py-3 px-4">{product.category}</td>
-                        <td className="py-3 px-4">{product.sold ? "Sold" :"Not Sold"}</td>
-                        {/* <td className="py-3 px-4">{product.image}</td> */}
-                    </tr>
-                ))}
-            </tbody>
-          </table>
-          <div className="flex justify-around my-4">
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg" onClick={handleDecrement}>
-              Previous
-            </button>
-            <span>{page}</span>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg" onClick={handleIncrement}>
-              Next
-            </button>
-          </div>
-        </div>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex justify-around my-4">
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg"
+          onClick={handleDecrement}
+        >
+          Previous
+        </button>
+        <span>{page}</span>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg"
+          onClick={handleIncrement}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
