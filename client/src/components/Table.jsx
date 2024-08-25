cb9bc415e8649f4f270ba433af4b67c1b0f652fc
nileshkharat-git <nileshk262000@gmail.com>
2024-08-25 09:30:03 +0530

    Update the search functionality

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,86 +1,76 @@
 import React, { useEffect, useState } from "react";
 
-const Table = () => {
+const Table = ({month}) => {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
+  const [searchText, setSearchText] = useState("");
+  const [isReadMore, setIsReadMore] = useState(true)
 
   const fetchData = async () => {
-    fetch(`http://localhost:8000/api/products?page=${page}`)
+    const url =
+      searchText.length > 0
+        ? `http://localhost:8000/api/products/${month}?page=${page}&search=${searchText}`
+        : `http://localhost:8000/api/products/${month}?page=${page}`;
+    fetch(url)
       .then((response) => response.json())
       .then((response) => setProducts(response))
       .catch((err) => console.error(err));
   };
+
   useEffect(() => {
     fetchData();
-  }, [page]);
+  }, [page, month]);
 
-  const fetchProducts = (page) => {
-    fetch(`http://localhost:8000/api/products?page=${page}`)
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        setProducts(response);
-      })
-      .catch((err) => console.error(err));
-  };
   const handleIncrement = () => {
     if (page < 6) {
       setPage((prev) => prev + 1);
-      fetchProducts(page);
+      fetchData();
     }
   };
   const handleDecrement = () => {
     if (page > 1) {
       setPage((prev) => prev - 1);
-      fetchProducts(page);
+      fetchData();
     }
   };
-  const months = [
-    { text: "Jan", value: 1 },
-    { text: "Feb", value: 2 },
-    { text: "Mar", value: 3 },
-    { text: "Apr", value: 4 },
-    { text: "May", value: 5 },
-    { text: "Jun", value: 6 },
-    { text: "Jul", value: 7 },
-    { text: "Aug", value: 8 },
-    { text: "Sep", value: 9 },
-    { text: "Oct", value: 10 },
-    { text: "Nov", value: 11 },
-    { text: "Dec", value: 12 },
-  ];
+
+  const handleSearch = () => {
+    fetchData();
+  };
+
+ 
   return (
     <div>
       <div className="flex items-center justify-between flex-col">
-        <div className="flex justify-between w-3/12 my-4">
+        <div className="flex w-5/12  my-4 ">
           <input
             type="text"
             id="first_name"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Search"
-            required
+            onChange={(e) => setSearchText(e.target.value)}
           />
-          <select className="outline-none focus:outline-none p-2 bg-white rounded-3xl">
-            {months.map((month) => (
-              <option key={month.value} value={month.value}>
-                {month.text}
-              </option>
-            ))}
-          </select>
+          <button
+            className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg mx-3"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+         
         </div>
       </div>
 
       <div className="overflow-x-auto">
-        <table className="w-full bg-white shadow-md rounded-xl mx-4">
+        <table className=" bg-white shadow-md rounded-xl mx-8">
           <thead>
             <tr className="bg-blue-gray-100 text-gray-700 uppercase">
               <th className="py-3 px-4 text-left">Id</th>
               <th className="py-3 px-4 text-left">Title</th>
-              {/* <th className="py-3 px-4 text-left">Descrition</th> */}
+              <th className="py-3 px-4 text-left">Descrition</th>
               <th className="py-3 px-4 text-left">Price</th>
               <th className="py-3 px-4 text-left">Category</th>
               <th className="py-3 px-4 text-left">Sold</th>
-              {/* <th className="py-3 px-4 text-left">Image</th> */}
+              <th className="py-3 px-4 text-left">Image</th>
             </tr>
           </thead>
           <tbody className="text-blue-gray-900">
@@ -88,13 +78,13 @@ const Table = () => {
               <tr className="border-b border-blue-gray-200">
                 <td className="py-3 px-4">{product.id}</td>
                 <td className="py-3 px-4">{product.title}</td>
-                {/* <td className="py-3 px-4 "><p>{product.description}</p></td> */}
+                <td className="py-3 px-4 "><p>{isReadMore ? product.description.slice(0,60):product.description} <span className="font-bold text-blue-500" onClick={()=>setIsReadMore(!isReadMore)}>{isReadMore?"...read more":"Show less"}</span></p></td>
                 <td className="py-3 px-4">{product.price} &#8377;</td>
                 <td className="py-3 px-4">{product.category}</td>
                 <td className="py-3 px-4">
                   {product.sold ? "Sold" : "Not Sold"}
                 </td>
-                {/* <td className="py-3 px-4">{product.image}</td> */}
+                <td className="py-3 px-4"><img src={product.image} alt="" className="w-52 object-fit"/></td>
               </tr>
             ))}
           </tbody>
