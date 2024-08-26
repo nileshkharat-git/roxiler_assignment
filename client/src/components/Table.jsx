eac6c3645338fb79b247efb429da738ba450f87e
nileshkharat-git <nileshk262000@gmail.com>
2024-08-26 10:18:24 +0530

    Add message for 'no products found'

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,10 +1,65 @@
 import React, { useEffect, useState } from "react";
 
-const Table = ({month}) => {
+const DataTable = ({ products, isReadMore, setIsReadMore }) => {
+  if (products.length == 0) {
+    return (
+      <div className="text-center my-8">
+        <h2 className="text-2xl font-bold text-gray-900">No Products Found</h2>
+      </div>
+    );
+  }
+  return (
+    <div className="overflow-x-auto">
+      <table className=" bg-white shadow-md rounded-xl mx-8">
+        <thead>
+          <tr className="bg-blue-gray-100 text-gray-700 uppercase">
+            <th className="py-3 px-4 text-left">Id</th>
+            <th className="py-3 px-4 text-left">Title</th>
+            <th className="py-3 px-4 text-left">Descrition</th>
+            <th className="py-3 px-4 text-left">Price</th>
+            <th className="py-3 px-4 text-left">Category</th>
+            <th className="py-3 px-4 text-left">Sold</th>
+            <th className="py-3 px-4 text-left">Image</th>
+          </tr>
+        </thead>
+        <tbody className="text-blue-gray-900">
+          {products.map((product, key) => (
+            <tr className="border-b border-blue-gray-200" key={key}>
+              <td className="py-3 px-4">{product.id}</td>
+              <td className="py-3 px-4">{product.title}</td>
+              <td className="py-3 px-4 ">
+                <p>
+                  {isReadMore
+                    ? product.description.slice(0, 60)
+                    : product.description}{" "}
+                  <span
+                    className="font-bold text-blue-500"
+                    onClick={() => setIsReadMore(!isReadMore)}
+                  >
+                    {isReadMore ? "...read more" : "Show less"}
+                  </span>
+                </p>
+              </td>
+              <td className="py-3 px-4">{product.price} &#8377;</td>
+              <td className="py-3 px-4">{product.category}</td>
+              <td className="py-3 px-4">
+                {product.sold ? "Sold" : "Not Sold"}
+              </td>
+              <td className="py-3 px-4">
+                <img src={product.image} alt="" className="w-52 object-fit" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+const Table = ({ month }) => {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
   const [searchText, setSearchText] = useState("");
-  const [isReadMore, setIsReadMore] = useState(true)
+  const [isReadMore, setIsReadMore] = useState(true);
 
   const fetchData = async () => {
     const url =
@@ -38,7 +93,6 @@ const Table = ({month}) => {
     fetchData();
   };
 
- 
   return (
     <div>
       <div className="flex items-center justify-between flex-col">
@@ -56,40 +110,14 @@ const Table = ({month}) => {
           >
             Search
           </button>
-         
         </div>
       </div>
 
-      <div className="overflow-x-auto">
-        <table className=" bg-white shadow-md rounded-xl mx-8">
-          <thead>
-            <tr className="bg-blue-gray-100 text-gray-700 uppercase">
-              <th className="py-3 px-4 text-left">Id</th>
-              <th className="py-3 px-4 text-left">Title</th>
-              <th className="py-3 px-4 text-left">Descrition</th>
-              <th className="py-3 px-4 text-left">Price</th>
-              <th className="py-3 px-4 text-left">Category</th>
-              <th className="py-3 px-4 text-left">Sold</th>
-              <th className="py-3 px-4 text-left">Image</th>
-            </tr>
-          </thead>
-          <tbody className="text-blue-gray-900">
-            {products.map((product, key) => (
-              <tr className="border-b border-blue-gray-200" key={key}>
-                <td className="py-3 px-4">{product.id}</td>
-                <td className="py-3 px-4">{product.title}</td>
-                <td className="py-3 px-4 "><p>{isReadMore ? product.description.slice(0,60):product.description} <span className="font-bold text-blue-500" onClick={()=>setIsReadMore(!isReadMore)}>{isReadMore?"...read more":"Show less"}</span></p></td>
-                <td className="py-3 px-4">{product.price} &#8377;</td>
-                <td className="py-3 px-4">{product.category}</td>
-                <td className="py-3 px-4">
-                  {product.sold ? "Sold" : "Not Sold"}
-                </td>
-                <td className="py-3 px-4"><img src={product.image} alt="" className="w-52 object-fit"/></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <DataTable
+        products={products}
+        isReadMore={isReadMore}
+        setIsReadMore={setIsReadMore}
+      />
       <div className="flex justify-around my-4">
         <button
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg"
